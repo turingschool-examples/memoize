bad5e305941fed5b3fef6ae7aa187db0805e1163
Justin Torrance <justin.torrance91@gmail.com>
2018-11-02 12:11:27 -0600

    Chance isTrue to isCorrect

--- datasets/codewiz-data.js
+++ datasets/codewiz-data.js
@@ -1,13 +1,13 @@
-const categories = {
+const codeWizCategories = {
   "scopeQuestions": [{
       "question": "A program that executes JavaScript code. Most commonly used in web browsers.",
       "answers": [{
           "answer": "Interpreter",
-          "isTrue": true
+          "isCorrect": true
         },
         {
           "answer": "Operating System",
-          "isTrue": false
+          "isCorrect": false
         }
       ]
     },
@@ -15,11 +15,11 @@ const categories = {
       "question": "Define scope",
       "answers": [{
           "answer": "The level in which a variable can be accessed",
-          "isTrue": true
+          "isCorrect": true
         },
         {
           "answer": "The length of the array",
-          "isTrue": false
+          "isCorrect": false
         }
       ]
     },
@@ -27,11 +27,11 @@ const categories = {
       "question": "An inner function that has access to (aka closes over) the variables declared outside itself.",
       "answers": [{
           "answer": "Closure",
-          "isTrue": true
+          "isCorrect": true
         },
         {
           "answer": "Hoisting",
-          "isTrue": false
+          "isCorrect": false
         }
       ]
     },
@@ -39,11 +39,11 @@ const categories = {
       "question": "The process of implicitly moving the declaration of variables and functions to the top of their scope.",
       "answers": [{
           "answer": "Hoisting",
-          "isTrue": true
+          "isCorrect": true
         },
         {
           "answer": "Operating System",
-          "isTrue": false
+          "isCorrect": false
         }
       ]
     },
@@ -51,11 +51,11 @@ const categories = {
       "question": "Which two keywords allow a variable to be block scoped?",
       "answers": [{
           "answer": "let, const",
-          "isTrue": true
+          "isCorrect": true
         },
         {
           "answer": "var, const",
-          "isTrue": false
+          "isCorrect": false
         }
       ]
     },
@@ -63,11 +63,11 @@ const categories = {
       "question": "True or false: Functions and variables in the global scope are “vulnerable” because they can be accessed by everything and potentially mutated.",
       "answers": [{
           "answer": "True",
-          "isTrue": true
+          "isCorrect": true
         },
         {
           "answer": "False",
-          "isTrue": false
+          "isCorrect": false
         }
       ]
     },
@@ -75,11 +75,11 @@ const categories = {
       "question": "What is considered a block statement?",
       "answers": [{
           "answer": "An if statement",
-          "isTrue": true
+          "isCorrect": true
         },
         {
           "answer": "A function",
-          "isTrue": false
+          "isCorrect": false
         }
       ]
     },
@@ -87,11 +87,11 @@ const categories = {
       "question": "Suppose you declare a variable in a for loop and you need that variable to stay scoped within the loop. Which of the following keywords should you use?",
       "answers": [{
           "answer": "let",
-          "isTrue": true
+          "isCorrect": true
         },
         {
           "answer": "var",
-          "isTrue": false
+          "isCorrect": false
         }
       ]
     },
@@ -99,11 +99,11 @@ const categories = {
       "question": "True or false: Child scopes have access to parent scopes, but not vice versa.",
       "answers": [{
           "answer": "True",
-          "isTrue": true
+          "isCorrect": true
         },
         {
           "answer": "True",
-          "isTrue": false
+          "isCorrect": false
         }
       ]
     },
@@ -111,11 +111,11 @@ const categories = {
       "question": "Variables declard in the global scope are added as properties of the ______.",
       "answers": [{
           "answer": "Window",
-          "isTrue": true
+          "isCorrect": true
         },
         {
           "answer": "Interpreter",
-          "isTrue": false
+          "isCorrect": false
         }
       ]
     }
@@ -125,11 +125,11 @@ const categories = {
       "question": "Which of the following prototype methods return an array of the same length as the original array?",
       "answers": [{
           "answer": "Map",
-          "isTrue": true
+          "isCorrect": true
         },
         {
           "answer": "Reduce",
-          "isTrue": false
+          "isCorrect": false
         }
       ]
     },
@@ -137,11 +137,11 @@ const categories = {
       "question": "Which of the following array prototype methods are mutator methods?",
       "answers": [{
           "answer": "Pop, push, shift, unshift",
-          "isTrue": true
+          "isCorrect": true
         },
         {
           "answer": "Map, reduce, filter, find",
-          "isTrue": false
+          "isCorrect": false
         }
       ]
     },
@@ -149,11 +149,11 @@ const categories = {
       "question": "Which method is capable of returning an object?",
       "answers": [{
           "answer": "Reduce",
-          "isTrue": true
+          "isCorrect": true
         },
         {
           "answer": "Find",
-          "isTrue": false
+          "isCorrect": false
         }
       ]
     },
@@ -161,11 +161,11 @@ const categories = {
       "question": "Which method returns a subset of the original array containing only the elements that match a given condition?",
       "answers": [{
           "answer": "Filter",
-          "isTrue": true
+          "isCorrect": true
         },
         {
           "answer": "forEach",
-          "isTrue": false
+          "isCorrect": false
         }
       ]
     },
@@ -173,11 +173,11 @@ const categories = {
       "question": "Which method returns the first element which matches a given condition?",
       "answers": [{
           "answer": "Find",
-          "isTrue": true
+          "isCorrect": true
         },
         {
           "answer": "Object.keys",
-          "isTrue": false
+          "isCorrect": false
         }
       ]
     },
@@ -185,11 +185,11 @@ const categories = {
       "question": "Which method returns an array of an object's keys?",
       "answers": [{
           "answer": "Object.keys",
-          "isTrue": true
+          "isCorrect": true
         },
         {
           "answer": "Object.assign",
-          "isTrue": false
+          "isCorrect": false
         }
       ]
     },
@@ -197,11 +197,11 @@ const categories = {
       "question": "Which method can make a copy of an existing object or merge multiple objects into a new object?",
       "answers": [{
           "answer": "Object.assign",
-          "isTrue": true
+          "isCorrect": true
         },
         {
           "answer": "Object.values",
-          "isTrue": false
+          "isCorrect": false
         }
       ]
     },
@@ -209,11 +209,11 @@ const categories = {
       "question": "Which method returns a copy of a portion of an existing array?",
       "answers": [{
           "answer": "Slice",
-          "isTrue": true
+          "isCorrect": true
         },
         {
           "answer": "Splice",
-          "isTrue": false
+          "isCorrect": false
         }
       ]
     },
@@ -221,11 +221,11 @@ const categories = {
       "question": "Which type of method loops through the existing array and applies a callback function that may mutate each element and return a new value?",
       "answers": [{
           "answer": "Iterator",
-          "isTrue": true
+          "isCorrect": true
         },
         {
           "answer": "Accessor",
-          "isTrue": false
+          "isCorrect": false
         }
       ]
     },
@@ -233,11 +233,11 @@ const categories = {
       "question": "Which array prototype method is an iterator which return undefined?",
       "answers": [{
           "answer": "forEach",
-          "isTrue": true
+          "isCorrect": true
         },
         {
           "answer": "Map",
-          "isTrue": false
+          "isCorrect": false
         }
       ]
     }
@@ -247,11 +247,11 @@ const categories = {
       "question": "True or false: The value of 'this' can only be changed inside of a function.",
       "answers": [{
           "answer": "True",
-          "isTrue": true
+          "isCorrect": true
         },
         {
           "answer": "False",
-          "isTrue": false
+          "isCorrect": false
         }
       ]
     },
@@ -259,11 +259,11 @@ const categories = {
       "question": "True or false: 'this' refers to the current context or owner of the code being executed.",
       "answers": [{
           "answer": "True",
-          "isTrue": true
+          "isCorrect": true
         },
         {
           "answer": "False",
-          "isTrue": false
+          "isCorrect": false
         }
       ]
     },
@@ -271,11 +271,11 @@ const categories = {
       "question": "True or false: 'this' refers to the object on which the current function is called.",
       "answers": [{
           "answer": "True",
-          "isTrue": true
+          "isCorrect": true
         },
         {
           "answer": "False",
-          "isTrue": false
+          "isCorrect": false
         }
       ]
     },
@@ -283,11 +283,11 @@ const categories = {
       "question": "By default, 'this' refers to _____.",
       "answers": [{
           "answer": "The window, or the global object",
-          "isTrue": true
+          "isCorrect": true
         },
         {
           "answer": "The interpreter",
-          "isTrue": false
+          "isCorrect": false
         }
       ]
     },
@@ -295,11 +295,11 @@ const categories = {
       "question": "True or false: In ES5, the value of 'this' is set when the function is invoked.",
       "answers": [{
           "answer": "True",
-          "isTrue": true
+          "isCorrect": true
         },
         {
           "answer": "False",
-          "isTrue": false
+          "isCorrect": false
         }
       ]
     },
@@ -307,11 +307,11 @@ const categories = {
       "question": "True or false: In ES6, the value of 'this' is set when the function is declared.",
       "answers": [{
           "answer": "True",
-          "isTrue": true
+          "isCorrect": true
         },
         {
           "answer": "False",
-          "isTrue": false
+          "isCorrect": false
         }
       ]
     },
@@ -319,11 +319,11 @@ const categories = {
       "question": "When using the keyword 'new', this refers to _____",
       "answers": [{
           "answer": "The newly created object",
-          "isTrue": true
+          "isCorrect": true
         },
         {
           "answer": "The window",
-          "isTrue": false
+          "isCorrect": false
         }
       ]
     },
@@ -331,11 +331,11 @@ const categories = {
       "question": "This function creates a new instance of an object.",
       "answers": [{
           "answer": "Constructor function",
-          "isTrue": true
+          "isCorrect": true
         },
         {
           "answer": "Super function",
-          "isTrue": false
+          "isCorrect": false
         }
       ]
     },
@@ -343,11 +343,11 @@ const categories = {
       "question": "True or false: In arrow functions, 'this' retains the value of the enclosing lexical context.",
       "answers": [{
           "answer": "True",
-          "isTrue": true
+          "isCorrect": true
         },
         {
           "answer": "False",
-          "isTrue": false
+          "isCorrect": false
         }
       ]
     },
@@ -355,11 +355,11 @@ const categories = {
       "question": "True or false: Context is most often determined by how a function is invoked.",
       "answers": [{
           "answer": "True",
-          "isTrue": true
+          "isCorrect": true
         },
         {
           "answer": "False",
-          "isTrue": false
+          "isCorrect": false
         }
       ]
     }
