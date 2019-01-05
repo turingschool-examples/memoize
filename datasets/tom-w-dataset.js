6a42b9298e057f612663e08c8a64255201063ae0
Tom Wilhoit <tomwilhoit.business@gmail.com>
2019-01-04 20:09:49 -0700

    Adds dataset

---
+++ datasets/tom-w-dataset.js
@@ -0,0 +1,282 @@
+flashCardDataSet = [{
+  "id": 1,
+  "question": "What is 'context' in Javascript?",
+  "correctAnswer": "The value of the this keyword, in reference to the object that owns the currently executing code",
+  "falseAnswer1": "Context is the exact same as scope",
+  "falseAnswer2": "Function based reference to the function that 'owns' the currently executing code"
+},
+{
+  "id": 2,
+  "question": "How do you access keys in an object?",
+  "correctAnswer": "Object.keys(objectName)",
+  "falseAnswer1": "Object.objectName(keys)",
+  "falseAnswer2": "Objection!"
+},
+{
+  "id": 3,
+  "question": "What is the difference between `const` and `let`?",
+  "correctAnswer": "With `const` the value cannot be reassigned",
+  "falseAnswer1": "With `let` the value cannot be reassigned",
+  "falseAnswer2": "Just `var` everything"
+},
+{
+  "id": 4,
+  "question": "How do you initialize a React application from the terminal?",
+  "correctAnswer": "npm install -g create-react-app",
+  "falseAnswer1": "npm install create-react-app",
+  "falseAnswer2": "npm react-start"
+},
+{
+  "id": 5,
+  "question": "What does 'linting' mean in code?",
+  "correctAnswer": "Linting is the process of running a program that will analyse code for potential errors",
+  "falseAnswer1": "Linting is the process of running a program that corrects functional errors",
+  "falseAnswer2": "Linting is the process of running a sticky roller over dark clothes"
+},
+{
+  "id": 6,
+  "question": "What does fetch() do?",
+  "correctAnswer": "It provides an easy, logical way to fetch resources asynchronously across the network.",
+  "falseAnswer1": "It is not going to happen",
+  "falseAnswer2": "It grabs methods and applies them to your current codebase"
+},
+{
+  "id": 7,
+  "question": "What does NPM mean?",
+  "correctAnswer": "It is the default package manager for the JavaScript runtime environment Node.js",
+  "falseAnswer1": "Node Packet Manager",
+  "falseAnswer2": "It is the environment in which dev tools are managed in the terminal"
+},
+{
+  "id": 8,
+  "question": "What is Babel?",
+  "correctAnswer": "A transpiler that converts ES6 to ES5 for older browsers",
+  "falseAnswer1": "A translation application for HTML and CSS",
+  "falseAnswer2": "A library for altering React into React Native"
+},
+{
+  "id": 9,
+  "question": "Which common array prototype returns a single value of any data type?",
+  "correctAnswer": "reduce()",
+  "falseAnswer1": "filter()",
+  "falseAnswer2": "forEach()"
+},
+{
+  "id": 10,
+  "question": "What does SRP stand for?",
+  "correctAnswer": "Single Responsibility Principle",
+  "falseAnswer1": "Single Recursion Principle",
+  "falseAnswer2": "Single Responsibility Prototype"
+},
+{
+  "id": 11,
+  "question": "What does SASS stand for?",
+  "correctAnswer": "Syntactically Awesome Style Sheets",
+  "falseAnswer1": "Sassy Artistic Style Sheets",
+  "falseAnswer2": "Super Asynchronous Style Sheets"
+},
+{
+  "id": 12,
+  "question": "What is a Chai spy?",
+  "correctAnswer": "A Chai ability to spy on the DOM for testing",
+  "falseAnswer1": "A Mocha ability to spy on Chai methods for testing",
+  "falseAnswer2": "The single most frustrating thing in history."
+},
+{
+  "id": 13,
+  "question": "What is the first rule of 'this' in relation to Javascript?",
+  "correctAnswer": "When executing a function as a method on an object, 'this' refers to that object.",
+  "falseAnswer1": "'this' in function code invoked using the new operator refers to the newly created object.",
+  "falseAnswer2": "Default 'this' refers to the global object"
+},
+{
+  "id": 14,
+  "question": "What is a function component in React?",
+  "correctAnswer": "A component passed a single set of 'props' and returns a React element",
+  "falseAnswer1": "A stateful component",
+  "falseAnswer2": "A component that changes the state of a sibling component"
+},
+{
+  "id": 15,
+  "question": "Is there a limit to how many components a React app can have?",
+  "correctAnswer": "No",
+  "falseAnswer1": "Yes",
+  "falseAnswer2": "There is a minimum of 5"
+},
+{
+  "id": 16,
+  "question": "What defines a class component in React?",
+  "correctAnswer": "A component that has 'state'",
+  "falseAnswer1": "A component passed a single set of 'props' and returns a React element",
+  "falseAnswer2": "A component that is an extension of JSX"
+},
+{
+  "id": 17,
+  "question": "What is the second rule of 'this' in relation to Javascript?",
+  "correctAnswer": "'this' in function code invoked using the new operator refers to the newly created object.",
+  "falseAnswer1": "When executing a function as a method on an object, 'this' refers to that object.",
+  "falseAnswer2": "Default 'this' refers to the global object"
+},
+{
+  "id": 18,
+  "question": "What are Mocha and Chai?",
+  "correctAnswer": "Pair of testing tools for Javascript",
+  "falseAnswer1": "Pair of testing tools specifically for React",
+  "falseAnswer2": "Pair of testing tools for the DOM"
+},
+{
+  "id": 19,
+  "question": "What is an effect of using `setState` in React?",
+  "correctAnswer": "Rerendering",
+  "falseAnswer1": "Reformatting",
+  "falseAnswer2": "Refactoring"
+},
+{
+  "id": 20,
+  "question": "What are props in React?",
+  "correctAnswer": "Properties able to be passed down to child components.",
+  "falseAnswer1": "Properties able to be passed up to parent components",
+  "falseAnswer2": "Properties able to be passed to sibling components"
+},
+{
+  "id": 21,
+  "question": "What does React lifecycle mean?",
+  "correctAnswer": "A cycle of a component created and mounted on the DOM to when it is unmounted and destroyed.",
+  "falseAnswer1": "A cycle of data created and destroyed in state.",
+  "falseAnswer2": "A cycle of returning values and not mounting them to the DOM"
+},
+{
+  "id": 22,
+  "question": "What does `data down/action up` mean in React?",
+  "correctAnswer": "Passing props and methods from parent to children, then utilizing the methods to send data back up.",
+  "falseAnswer1": "Sending data down the scope chain and returning values",
+  "falseAnswer2": "An organizational plan for your code ahead of actually implementing"
+},
+{
+  "id": 23,
+  "question": "What is a switch statement?",
+  "correctAnswer": "Conditional logic used to evaluate what code should be run",
+  "falseAnswer1": "A React lifecycle hook",
+  "falseAnswer2": "A move to flip-flop on political stances"
+},
+{
+  "id": 24,
+  "question": "What is conditional rendering mean in React?",
+  "correctAnswer": "Elements are rendered based on conditional logic",
+  "falseAnswer1": "Elements are rendered based on the state of sibling components",
+  "falseAnswer2": "Elements are rendered differently based on their SASS styling"
+},
+{
+  "id": 25,
+  "question": "Can you map() an object?",
+  "correctAnswer": "Negative, Ghostrider",
+  "falseAnswer1": "10-4 Good Buddy",
+  "falseAnswer2": "I have no idea how I got into this building"
+},
+{
+  "id": 26,
+  "question": "What is an accumulator?",
+  "correctAnswer": "The returned value in a reduce()",
+  "falseAnswer1": "The initial value in a reduce()",
+  "falseAnswer2": "The incrementor in a 'for loop'"
+},
+{
+  "id": 27,
+  "question": "What is the difference between find() and filter()?",
+  "correctAnswer": "Filter() returns all array elements that matches the conditional, find() returns only the first",
+  "falseAnswer1": "Filter() returns the first array element that matches the conditional, find() returns all that match",
+  "falseAnswer2": "Filter() returns all array elements that match the conditional, find() returns the last that matches."
+},
+{
+  "id": 28,
+  "question": "What is a `truthy` value?",
+  "correctAnswer": "A value that is considered true when encountered in a Boolean context",
+  "falseAnswer1": "A value that is considered undefined when encountered in a Boolean context",
+  "falseAnswer2": "A value that is considered false when encountered in a Boolean context"
+},
+{
+  "id": 29,
+  "question": "What is JSX?",
+  "correctAnswer": "Javascript XML",
+  "falseAnswer1": "JavaScript Experience",
+  "falseAnswer2": "A testing suite for React"
+},
+{
+  "id": 30,
+  "question": "What is ES6 destructoring?",
+  "correctAnswer": "An expression to unpack values from arrays, or properties from objects, into distinct variables.",
+  "falseAnswer1": "Breaking down CSS classes into functional rules",
+  "falseAnswer2": "The ability to use old browsers with current code updates"
+},
+{
+  "id": 31,
+  "question": "What does `state` mean in React?",
+  "correctAnswer": "The act of storing values in a component",
+  "falseAnswer1": "The current executing code of a component",
+  "falseAnswer2": "The difference in functionality in Vanilla JS"
+},
+{
+  "id": 32,
+  "question": "In what case might you use bracket notation?",
+  "correctAnswer": "When the key of an object might change",
+  "falseAnswer1": "When the key of an object cannot change",
+  "falseAnswer2": "When there is no key to the object"
+},
+{
+  "id": 33,
+  "question": "What is the difference between forking and cloning?",
+  "correctAnswer": "Forking creates a working clone of a repo for you to contribute to",
+  "falseAnswer1": "Cloning creates a working clone of a repo for you to contribute to",
+  "falseAnswer2": "They are the same thing"
+},
+{
+  "id": 34,
+  "question": "Which of the following are simple data types?",
+  "correctAnswer": "Boolean, number, string, null, undefined",
+  "falseAnswer1": "Boolean, number, string, null, array",
+  "falseAnswer2": "Boolean, string, null, undefined, object"
+},
+{
+  "id": 35,
+  "question": "What is Object Oriented Programming?",
+  "correctAnswer": "The methodology of programming around objects and classes",
+  "falseAnswer1": "Coding all functionality into one JS file",
+  "falseAnswer2": "Moving CSS files into your JS files "
+},
+{
+  "id": 36,
+  "question": "What is `this` in relation to Javascript?",
+  "correctAnswer": "Typically refers to the object which 'owns' the method",
+  "falseAnswer1": "A placeholder for the highest class in all JS files",
+  "falseAnswer2": "References a stored method in React"
+},
+{
+  "id": 37,
+  "question": "What are the 4 phases in Test Driven Development?",
+  "correctAnswer": "Setup, excercise, verify, teardown",
+  "falseAnswer1": "Setup, verify, exercise, teardown",
+  "falseAnswer2": "Verify, setup, exercise, teardown"
+},
+{
+  "id": 38,
+  "question": "What does immutable mean in relation to arrays?",
+  "correctAnswer": "An array that cannot be mutated",
+  "falseAnswer1": "An array that has been designed to mutate",
+  "falseAnswer2": "An array that can be mutated"
+},
+{
+  "id": 39,
+  "question": "What is a recursive function?",
+  "correctAnswer": "A function that calls itself",
+  "falseAnswer1": "An anonymous function",
+  "falseAnswer2": "A function that cannot be called"
+},
+{
+  "id": 40,
+  "question": "What is the favorite dog breed of Instructor Robbie?",
+  "correctAnswer": "Pugs",
+  "falseAnswer1": "Poodles",
+  "falseAnswer2": "Cats, actually"
+}
+]
+
