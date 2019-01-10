f7d9e41cb323ebc7b05083678fe362d695637861
Brittany Storoz <brittanystoroz@gmail.com>
2019-01-10 11:55:10 -0700

    Update dylan

--- datasets/flexbox.js
+++ datasets/flexbox.js
@@ -4,7 +4,8 @@ const flexboxData = [{
   "answers": ["https://i.imgur.com/Xo1MD86.png", "https://i.imgur.com/JIJHnVf.png", "https://i.imgur.com/4y9cZDw.png", "https://i.imgur.com/Id0kLlR.png"],
   "info": "This establishes the main-axis, thus defining the direction flex items are placed in the flex container. Using row organizes the items from left to right.",
   "family-member": "Parent",
-  "model": "Flexbox"
+  "model": "Flexbox",
+  "id": "bbbfc880-b021-43e7-92ac-efe96902644e"
 },
 {
   "property": "flex-direction: column",
@@ -12,7 +13,8 @@ const flexboxData = [{
   "answers": ["https://i.imgur.com/JIJHnVf.png", "https://i.imgur.com/Xo1MD86.png", "https://i.imgur.com/4y9cZDw.png", "https://i.imgur.com/VdZUG9d.png"],
   "info": "This establishes the main-axis, thus defining the direction flex items are placed in the flex container. Using Column organizes the items from top to bottom.",
   "family-member": "Parent",
-  "model": "Flexbox"
+  "model": "Flexbox",
+  "id": "6c3f6bf8-0720-46bb-93a7-7a83cf42a256"
 },
 {
   "property": "flex-direction: row-reverse",
@@ -20,7 +22,8 @@ const flexboxData = [{
   "answers": ["https://i.imgur.com/Id0kLlR.png", "https://i.imgur.com/JIJHnVf.png", "https://i.imgur.com/4y9cZDw.png", "https://i.imgur.com/Xo1MD86.png"],
   "info": "This establishes the main-axis, thus defining the direction flex items are placed in the flex container. Using row reverse organizes the items from right to left.",
   "family-member": "Parent",
-  "model": "Flexbox"
+  "model": "Flexbox",
+  "id": "dc5a8a63-762b-4e13-b1c3-a0e2bf2cf32e"
 },
 {
   "property": "flex-direction: column-reverse",
@@ -28,7 +31,8 @@ const flexboxData = [{
   "answers": ["https://i.imgur.com/VdZUG9d.png", "https://i.imgur.com/JIJHnVf.png", "https://i.imgur.com/Xo1MD86.png", "https://i.imgur.com/4y9cZDw.png"],
   "info": "This establishes the main-axis, thus defining the direction flex items are placed in the flex container. Using column reverse organizes the items from bottom to top.",
   "family-member": "Parent",
-  "model": "Flexbox"
+  "model": "Flexbox",
+  "id": "4926bd67-5187-4bda-b03a-d5f177a263c1"
 },
 {
   "property": "flex-wrap: no-wrap",
@@ -36,7 +40,8 @@ const flexboxData = [{
   "answers": ["https://i.imgur.com/T3rkSzF.png", "https://i.imgur.com/b9CPTJs.png", "https://i.imgur.com/mQzfZ3n.png", "https://i.imgur.com/JIJHnVf.png"],
   "info": "By default, flex items will all try to fit onto one line. You can change that and allow the items to wrap as needed with this property. No wrap means all items will be on one line.",
   "family-member": "Parent",
-  "model": "Flexbox"
+  "model": "Flexbox",
+  "id": "dc930014-314e-49d6-9377-2be0cf840432"
 },
 {
   "property": "flex-wrap: wrap",
@@ -44,7 +49,8 @@ const flexboxData = [{
   "answers": ["https://i.imgur.com/b9CPTJs.png", "https://i.imgur.com/T3rkSzF.png", "https://i.imgur.com/mQzfZ3n.png", "https://i.imgur.com/JIJHnVf.png"],
   "info": "By default, flex items will all try to fit onto one line. You can change that and allow the items to wrap as needed with this property. With wrap flex items will wrap onto multiple lines, from top to bottom.",
   "family-member": "Parent",
-  "model": "Flexbox"
+  "model": "Flexbox",
+  "id": "edcb82c8-d46e-4bfe-8ec2-c95ae70161f6"
 },
 {
   "property": "flex-wrap: wrap-reverse",
@@ -52,7 +58,8 @@ const flexboxData = [{
   "answers": ["https://i.imgur.com/mQzfZ3n.png", "https://i.imgur.com/T3rkSzF.png", "https://i.imgur.com/b9CPTJs.png", "https://i.imgur.com/JIJHnVf.png"],
   "info": "By default, flex items will all try to fit onto one line. You can change that and allow the items to wrap as needed with this property. With wrap-reverse flex items will wrap onto multiple lines from bottom to top.",
   "family-member": "Parent",
-  "model": "Flexbox"
+  "model": "Flexbox",
+  "id": "61b0b8d5-6f6d-4370-8c78-052c9f2fda22"
 },
 {
   "property": "justify-content: flex-start",
@@ -60,7 +67,8 @@ const flexboxData = [{
   "answers": ["https://i.imgur.com/irGUri3.png", "https://i.imgur.com/4y9cZDw.png", "https://i.imgur.com/diRO5NH.png", "https://i.imgur.com/tIf2JCm.png"],
   "info": "This defines the alignment along the main axis. It helps distribute extra free space left over when either all the flex items on a line are inflexible, or are flexible but have reached their maximum size. When you use flex-start items are packed toward the start line.",
   "family-member": "Parent",
-  "model": "Flexbox"
+  "model": "Flexbox",
+  "id": "6d74421e-07c3-4082-a1e5-92797aa6fe9b"
 },
 {
   "property": "justify-content: flex-end",
@@ -68,7 +76,8 @@ const flexboxData = [{
   "answers": ["https://i.imgur.com/4y9cZDw.png", "https://i.imgur.com/irGUri3.png", "https://i.imgur.com/diRO5NH.png", "https://i.imgur.com/x30QEEE.png"],
   "info": "This defines the alignment along the main axis. It helps distribute extra free space left over when either all the flex items on a line are inflexible, or are flexible but have reached their maximum size. When you use flex-end items are packed toward the end line.",
   "family-member": "Parent",
-  "model": "Flexbox"
+  "model": "Flexbox",
+  "id": "ee885fe6-3932-4de5-a125-ade3f0078d29"
 },
 {
   "property": "justify-content: center",
@@ -76,7 +85,8 @@ const flexboxData = [{
   "answers": ["https://i.imgur.com/diRO5NH.png", "https://i.imgur.com/4y9cZDw.png", "https://i.imgur.com/irGUri3.png", "https://i.imgur.com/x30QEEE.png"],
   "info": "This defines the alignment along the main axis. It helps distribute extra free space left over when either all the flex items on a line are inflexible, or are flexible but have reached their maximum size. When you use center items are centered along the line.",
   "family-member": "Parent",
-  "model": "Flexbox"
+  "model": "Flexbox",
+  "id": "615647e7-a9cc-4822-9af7-ad8082a1d6f8"
 },
 {
   "property": "justify-content: space-between",
@@ -84,7 +94,8 @@ const flexboxData = [{
   "answers": ["https://i.imgur.com/x30QEEE.png", "https://i.imgur.com/irGUri3.png", "https://i.imgur.com/4y9cZDw.png", "https://i.imgur.com/tIf2JCm.png"],
   "info": "This defines the alignment along the main axis. It helps distribute extra free space left over when either all the flex items on a line are inflexible, or are flexible but have reached their maximum size. When you use space-between items are evenly distributed along the line with the first item on the start line and the last item on the end line.",
   "family-member": "Parent",
-  "model": "Flexbox"
+  "model": "Flexbox",
+  "id": "89c1a05c-edd0-4041-837c-f65d1473d9b9"
 },
 {
   "property": "justify-content: space-around",
@@ -92,7 +103,8 @@ const flexboxData = [{
   "answers": ["https://i.imgur.com/tIf2JCm.png", "https://i.imgur.com/4y9cZDw.png", "https://i.imgur.com/diRO5NH.png", "https://i.imgur.com/x30QEEE.png"],
   "info": "This defines the alignment along the main axis. It helps distribute extra free space left over when either all the flex items on a line are inflexible, or are flexible but have reached their maximum size. When you use space-around items are evenly distributed along the line with equal space around them. Note that visually the spaces aren't equal, since all the items have equal space on both sides. The first item will have one unit of space against the container edge, but two units of space between the next item because that next item has its own spacing that applies.",
   "family-member": "Parent",
-  "model": "Flexbox"
+  "model": "Flexbox",
+  "id": "265874a9-7224-4b8a-945b-e455579bb2da"
 },
 {
   "property": "justify-content: space-evenly",
@@ -100,15 +112,17 @@ const flexboxData = [{
   "answers": ["https://i.imgur.com/tIf2JCm.png", "https://i.imgur.com/x30QEEE.png", "https://i.imgur.com/tIf2JCm.png", "https://i.imgur.com/irGUri3.png"],
   "info": "This defines the alignment along the main axis. It helps distribute extra free space left over when either all the flex items on a line are inflexible, or are flexible but have reached their maximum size. When you use space-evenly  items are distributed so that the spacing between any two items (and the space to the edges) is equal..",
   "family-member": "Parent",
-  "model": "Flexbox"
+  "model": "Flexbox",
+  "id": "142bd5e7-d172-4bdc-8831-fa283dc58a14"
 },
 {
   "property": "align-items: flex-start",
   "correctAnswer": "https://i.imgur.com/wG3sXfv.png",
-  "answers": ["https://i.imgur.com/jwQ4wSd.png", "https://i.imgur.com/aqGjnBq.png", "https://i.imgur.com/lLOSTyE.png"],
+  "answers": ["https://i.imgur.com/wG3sXfv.png","https://i.imgur.com/jwQ4wSd.png", "https://i.imgur.com/aqGjnBq.png", "https://i.imgur.com/lLOSTyE.png"],
   "info": "This defines the default behavior for how flex items are laid out along the cross axis on the current line. Using flex-start will align the cross-start margin edge of the items is placed on the cross-start line.",
   "family-member": "Parent",
-  "model": "Flexbox"
+  "model": "Flexbox",
+  "id": "90f1b152-b3c8-461f-8080-1cb74fd1022b"
 },
 {
   "property": "align-items: flex-end",
@@ -116,7 +130,8 @@ const flexboxData = [{
   "answers": ["https://i.imgur.com/jwQ4wSd.png", "https://i.imgur.com/wG3sXfv.png", "https://i.imgur.com/2vbrxxs.png", "https://i.imgur.com/aqGjnBq.png"],
   "info": "This defines the default behavior for how flex items are laid out along the cross axis on the current line. Using flex-end will align the cross-end margin edge of the items is placed on the cross-end line.",
   "family-member": "Parent",
-  "model": "Flexbox"
+  "model": "Flexbox",
+  "id": "1c2c015a-5fe5-4c98-a34b-92f95b68e211"
 },
 {
   "property": "align-items: center",
@@ -124,7 +139,8 @@ const flexboxData = [{
   "answers": ["https://i.imgur.com/aqGjnBq.png", "https://i.imgur.com/wG3sXfv.png", "https://i.imgur.com/jwQ4wSd.png", "https://i.imgur.com/2vbrxxs.png"],
   "info": "This defines the default behavior for how flex items are laid out along the cross axis on the current line. Using center will center items in the cross-axis.",
   "family-member": "Parent",
-  "model": "Flexbox"
+  "model": "Flexbox",
+  "id": "9d112a01-2f4a-4a47-a2e1-8108d2ef38ae"
 },
 {
   "property": "align-items: stretch",
@@ -132,7 +148,8 @@ const flexboxData = [{
   "answers": ["https://i.imgur.com/2vbrxxs.png", "https://i.imgur.com/aqGjnBq.png", "https://i.imgur.com/wG3sXfv.png", "https://i.imgur.com/lLOSTyE.png"],
   "info": "This defines the default behavior for how flex items are laid out along the cross axis on the current line. When you use stretch, your items will stretch to fill the container. Note: Specific heights will over-ride stretch",
   "family-member": "Parent",
-  "model": "Flexbox"
+  "model": "Flexbox",
+  "id": "1ce6a6d6-c72a-4bb0-b9de-db201e26fee0"
 },
 {
   "property": "align-items: baseline",
@@ -140,7 +157,8 @@ const flexboxData = [{
   "answers": ["https://i.imgur.com/lLOSTyE.png", "https://i.imgur.com/wG3sXfv.png", "https://i.imgur.com/aqGjnBq.png", "https://i.imgur.com/jwQ4wSd.png"],
   "info": "This defines the default behavior for how flex items are laid out along the cross axis on the current line. When you use baseline, your items are aligned such as their baselines align.",
   "family-member": "Parent",
-  "model": "Flexbox"
+  "model": "Flexbox",
+  "id": "ba71edf9-5f35-4346-87ca-5876521514d8"
 },
 {
   "property": "align-content: flex-start",
@@ -148,7 +166,8 @@ const flexboxData = [{
   "answers": ["https://i.imgur.com/LDWGOm6.png", "https://i.imgur.com/FRwBgsu.png", "https://i.imgur.com/WyfHi6D.png", "https://i.imgur.com/LV6tkjy.png"],
   "info": "This aligns a flex container's lines within when there is extra space in the cross-axis, similar to how justify-content aligns individual items within the main-axis. Using flex-start pushes your lines to the start of the container. Note: this property has no effect when there is only one line of flex items.",
   "family-member": "Parent",
-  "model": "Flexbox"
+  "model": "Flexbox",
+  "id": "8370ecb6-3532-4912-a99d-91f8107dbf9f"
 },
 {
   "property": "align-content: flex-end",
@@ -156,7 +175,8 @@ const flexboxData = [{
   "answers": ["https://i.imgur.com/FRwBgsu.png", "https://i.imgur.com/LDWGOm6.png", "https://i.imgur.com/mwOL8UU.png", "https://i.imgur.com/UWvwKca.png"],
   "info": "This aligns a flex container's lines within when there is extra space in the cross-axis, similar to how justify-content aligns individual items within the main-axis. Using flex-end pushes your lines to the end of the container. Note: this property has no effect when there is only one line of flex items.",
   "family-member": "Parent",
-  "model": "Flexbox"
+  "model": "Flexbox",
+  "id": "eb6dd22c-9687-4ff5-a0b3-29148fc15606"
 },
 {
   "property": "align-content: center",
@@ -164,7 +184,8 @@ const flexboxData = [{
   "answers": ["https://i.imgur.com/WyfHi6D.png", "https://i.imgur.com/FRwBgsu.png", "https://i.imgur.com/LV6tkjy.png", "https://i.imgur.com/LDWGOm6.png"],
   "info": "This aligns a flex container's lines within when there is extra space in the cross-axis, similar to how justify-content aligns individual items within the main-axis. Using center packs your lines to the center of the container. Note: this property has no effect when there is only one line of flex items.",
   "family-member": "Parent",
-  "model": "Flexbox"
+  "model": "Flexbox",
+  "id": "de7a1c35-f2da-4241-b266-74181dc1ea2c"
 },
 {
   "property": "align-content: space-between",
@@ -172,7 +193,8 @@ const flexboxData = [{
   "answers": ["https://i.imgur.com/LV6tkjy.png", "https://i.imgur.com/WyfHi6D.png", "https://i.imgur.com/mwOL8UU.png", "https://i.imgur.com/FRwBgsu.png"],
   "info": "This aligns a flex container's lines within when there is extra space in the cross-axis, similar to how justify-content aligns individual items within the main-axis. Using space-between evenly distributes with the first line at the start of the container while the last one is at the end. Note: this property has no effect when there is only one line of flex items.",
   "family-member": "Parent",
-  "model": "Flexbox"
+  "model": "Flexbox",
+  "id": "2d92d6e8-575c-4555-88b7-4dabb72f9ba3"
 },
 {
   "property": "align-content: space-around",
@@ -180,7 +202,8 @@ const flexboxData = [{
   "answers": ["https://i.imgur.com/mwOL8UU.png", "https://i.imgur.com/UWvwKca.png", "https://i.imgur.com/UWvwKca.png", "https://i.imgur.com/LV6tkjy.png"],
   "info": "This aligns a flex container's lines within when there is extra space in the cross-axis, similar to how justify-content aligns individual items within the main-axis. Using space-around evenly distributes your lines with equal space around each line. Note: this property has no effect when there is only one line of flex items.",
   "family-member": "Parent",
-  "model": "Flexbox"
+  "model": "Flexbox",
+  "id": "b19bff0e-c654-4e4f-8a87-5f30e88d25af"
 },
 {
   "property": "align-content: stretch",
@@ -188,7 +211,8 @@ const flexboxData = [{
   "answers": ["https://i.imgur.com/UWvwKca.png", "https://i.imgur.com/mwOL8UU.png", "https://i.imgur.com/WyfHi6D.png", "https://i.imgur.com/LDWGOm6.png"],
   "info": "This aligns a flex container's lines within when there is extra space in the cross-axis, similar to how justify-content aligns individual items within the main-axis. Using stretch will stretch your lines to take up the remaining space. Note: this property has no effect when there is only one line of flex items. Specific heights will over-ride stretch.",
   "family-member": "Parent",
-  "model": "Flexbox"
+  "model": "Flexbox",
+  "id": "6eec05c6-1750-4eca-8388-815f42884f28"
 },
 {
   "property": "order: <integer>",
@@ -196,7 +220,8 @@ const flexboxData = [{
   "answers": ["https://i.imgur.com/OQXwgGo.png", "https://i.imgur.com/tMNZuBu.png", "https://i.imgur.com/k8o3I51.png", "https://i.imgur.com/Y4KmclT.png"],
   "info": "By default, flex items are laid out in the source order. However, the order property controls the order in which they appear in the flex container.",
   "family-member": "Child",
-  "model": "Flexbox"
+  "model": "Flexbox",
+  "id": "4ed6cc5e-492b-413d-9875-c4dab2457466"
 },
 {
   "property": "flex-grow: <number>",
@@ -204,7 +229,8 @@ const flexboxData = [{
   "answers": ["https://i.imgur.com/tMNZuBu.png", "https://i.imgur.com/OQXwgGo.png", "https://i.imgur.com/EWMExs2.png", "https://i.imgur.com/JHG2zvj.png"],
   "info": "This defines the ability for a flex item to grow if necessary. It accepts a unitless value that serves as a proportion. It dictates what amount of the available space inside the flex container the item should take up. If all items have flex-grow set to 1, the remaining space in the container will be distributed equally to all children. If one of the children has a value of 2, the remaining space would take up twice as much space as the others (or it will try to, at least).",
   "family-member": "Child",
-  "model": "Flexbox"
+  "model": "Flexbox",
+  "id": "ca43f681-48a6-490e-bff1-97cd06f69998"
 },
 {
   "property": "flex-shrink: <number>",
@@ -212,7 +238,8 @@ const flexboxData = [{
   "answers": ["https://i.imgur.com/k8o3I51.png", "https://i.imgur.com/tMNZuBu.png", "https://i.imgur.com/OQXwgGo.png", "https://i.imgur.com/Y4KmclT.png"],
   "info": "This defines the ability for a flex item to shrink if necessary.",
   "family-member": "Child",
-  "model": "Flexbox"
+  "model": "Flexbox",
+  "id": "1dcf82c4-e34c-47c7-84b8-aef5d11c6984"
 },
 {
   "property": "align-self: flex-start",
@@ -220,7 +247,8 @@ const flexboxData = [{
   "answers": ["https://i.imgur.com/Y4KmclT.png", "https://i.imgur.com/OQXwgGo.png", "https://i.imgur.com/JHG2zvj.png", "https://i.imgur.com/tMNZuBu.png"],
   "info": "This allows the default alignment (or the one specified by align-items) to be overridden for individual flex items. Using flex start will align the item at the start of the container.",
   "family-member": "Child",
-  "model": "Flexbox"
+  "model": "Flexbox",
+  "id": "0048d374-f6b1-4d81-89b4-5ed726f436f9"
 },
 {
   "property": "align-self: flex-end",
@@ -228,7 +256,8 @@ const flexboxData = [{
   "answers": ["https://i.imgur.com/JHG2zvj.png", "https://i.imgur.com/Y4KmclT.png", "https://i.imgur.com/OQXwgGo.png", "https://i.imgur.com/EWMExs2.png"],
   "info": "This allows the default alignment (or the one specified by align-items) to be overridden for individual flex items. Using flex end will align the item at the end of the container.",
   "family-member": "Child",
-  "model": "Flexbox"
+  "model": "Flexbox",
+  "id": "2a5a84be-81ce-4e38-92dc-c5ab1d790297"
 },
 {
   "property": "align-self: center",
@@ -236,7 +265,8 @@ const flexboxData = [{
   "answers": ["https://i.imgur.com/EWMExs2.png", "https://i.imgur.com/Y4KmclT.png", "https://i.imgur.com/OQXwgGo.png", "https://i.imgur.com/KP563vf.png"],
   "info": "This allows the default alignment (or the one specified by align-items) to be overridden for individual flex items. Using center will center the item in your container.",
   "family-member": "Child",
-  "model": "Flexbox"
+  "model": "Flexbox",
+  "id": "b259c7fa-febb-4963-ac7b-758bbaa35df8"
 },
 {
   "property": "align-self: stretch",
@@ -244,7 +274,8 @@ const flexboxData = [{
   "answers": ["https://i.imgur.com/KP563vf.png", "https://i.imgur.com/JHG2zvj.png", "https://i.imgur.com/EWMExs2.png", "https://i.imgur.com/JHG2zvj.png"],
   "info": "This allows the default alignment (or the one specified by align-items) to be overridden for individual flex items. Using stretch will stretch the item to fill your container. Note: A specific heights will over-ride stretch.",
   "family-member": "Child",
-  "model": "Flexbox"
+  "model": "Flexbox",
+  "id": "2970020f-8f4b-4eb9-8b7d-dedee6c18325"
 }]
 
 module.exports = { flexboxData }; 
