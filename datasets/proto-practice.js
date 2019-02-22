9d97c5c8bc402b667255c3a16ddc2fbab5421a24
jessicalynhansen@gmail.com <jessicalynhansen@gmail.com>
2019-02-22 12:11:00 -0700

    Add Jessica's dataset

---
+++ datasets/proto-practice.js
@@ -0,0 +1,366 @@
+const proto-practice = [
+    {
+      "id": "a728f57a-36c6-11e9-b210-d663bd873d93",
+      "category": "Array Prototype",
+      "img": "images/reduce.png",
+      "correctAnswer": "reduce",
+      "answerList": [
+        "find",
+        "reduce",
+        "map"
+        ],
+      "docs": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce"
+    },
+    {
+      "id": "a728f94e-36c6-11e9-b210-d663bd873d93",
+      "category": "Array Prototype",
+      "img": "images/length.png",
+      "correctAnswer": "length",
+      "answerList": [
+        "length",
+        "indexOf",
+        "split"
+        ],
+      "docs": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/length"
+    },
+    {
+      "id": "a728fb60-36c6-11e9-b210-d663bd873d93",
+      "category": "Array Prototype",
+      "img": "images/concat.png",
+      "correctAnswer": "concat",
+      "answerList": [
+        "concat",
+        "shift",
+        "forEach"
+        ],
+      "docs": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/concat"
+    },
+    {
+      "id": "a728fe26-36c6-11e9-b210-d663bd873d93",
+      "category": "Array Prototype",
+      "img": "images/filter.png",
+      "correctAnswer": "filter",
+      "answerList": [
+        "find",
+        "filter",
+        "forEach"
+        ],
+      "docs": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter"
+    },
+    {
+      "id": "a728fff2-36c6-11e9-b210-d663bd873d93",
+      "category": "Array Prototype",
+      "img": "images/find.png",
+      "correctAnswer": "find",
+      "answerList": [
+        "join",
+        "concat",
+        "find"
+        ],
+      "docs": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find"
+    },
+    {
+      "id": "a72901e6-36c6-11e9-b210-d663bd873d93",
+      "category": "Array Prototype",
+      "img": "images/flat.png",
+      "correctAnswer": "flat",
+      "answerList": [
+        "flat",
+        "reverse",
+        "toString"
+        ],
+      "docs": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flat"
+    },
+    {
+      "id": "a7290394-36c6-11e9-b210-d663bd873d93",
+      "category": "Array Prototype",
+      "img": "images/forEach.png",
+      "correctAnswer": "forEach",
+      "answerList": [
+        "pop",
+        "reduce",
+        "forEach"
+        ],
+      "docs": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach"
+    },
+    {
+      "id": "a7290538-36c6-11e9-b210-d663bd873d93",
+      "category": "Array Prototype",
+      "img": "images/includes.png",
+      "correctAnswer": "includes",
+      "answerList": [
+        "includes",
+        "find",
+        "reverse"
+        ],
+      "docs": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes"
+    },
+    {
+      "id": "a729079a-36c6-11e9-b210-d663bd873d93",
+      "category": "Array Prototype",
+      "img": "images/indexOf.png",
+      "correctAnswer": "indexOf",
+      "answerList": [
+        "includes",
+        "find",
+        "indexOf"
+        ],
+      "docs": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf"
+    },
+    {
+      "id": "a72909ac-36c6-11e9-b210-d663bd873d93",
+      "category": "Array Prototype",
+      "img": "images/join.png",
+      "correctAnswer": "join",
+      "answerList": [
+        "join",
+        "concat",
+        "push"
+        ],
+      "docs": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join"
+    },
+    {
+      "id": "a7290bd2-36c6-11e9-b210-d663bd873d93",
+      "category": "Array Prototype",
+      "img": "images/map.png",
+      "correctAnswer": "map",
+      "answerList": [
+        "reduce",
+        "map",
+        "filter"
+        ],
+      "docs": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map"
+    },
+    {
+      "id": "a7290dbc-36c6-11e9-b210-d663bd873d93",
+      "category": "Array Prototype",
+      "img": "images/pop.png",
+      "correctAnswer": "pop",
+      "answerList": [
+        "shift",
+        "push",
+        "pop"
+        ],
+      "docs": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/pop"
+    },
+    {
+      "id": "a7290fc4-36c6-11e9-b210-d663bd873d93",
+      "category": "Array Prototype",
+      "img": "images/push.png",
+      "correctAnswer": "push",
+      "answerList": [
+        "push",
+        "reverse",
+        "pop"
+        ],
+      "docs": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push"
+    },
+    {
+      "id": "a729124e-36c6-11e9-b210-d663bd873d93",
+      "category": "Array Prototype",
+      "img": "images/reverse.png",
+      "correctAnswer": "reverse",
+      "answerList": [
+        "shift",
+        "reverse",
+        "filter"
+        ],
+      "docs": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reverse"
+    },
+    {
+      "id": "a72913de-36c6-11e9-b210-d663bd873d93",
+      "category": "Array Prototype",
+      "img": "images/shift.png",
+      "correctAnswer": "shift",
+      "answerList": [
+        "push",
+        "includes",
+        "shift"
+        ],
+      "docs": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/shift"
+    },
+    {
+      "id": "a7291596-36c6-11e9-b210-d663bd873d93",
+      "category": "Array Prototype",
+      "img": "images/slice.png",
+      "correctAnswer": "slice",
+      "answerList": [
+        "shift",
+        "slice",
+        "pop"
+        ],
+      "docs": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice"
+    },
+    {
+      "id": "a72917c6-36c6-11e9-b210-d663bd873d93",
+      "category": "Array Prototype",
+      "img": "images/sort.png",
+      "correctAnswer": "sort",
+      "answerList": [
+        "shift",
+        "reverse",
+        "sort"
+        ],
+      "docs": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort"
+    },
+    {
+      "id": "a72919d8-36c6-11e9-b210-d663bd873d93",
+      "category": "Array Prototype",
+      "img": "images/splice.png",
+      "correctAnswer": "splice",
+      "answerList": [
+        "shift",
+        "sort",
+        "splice"
+        ],
+      "docs": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice"
+    },
+    {
+      "id": "a7291c58-36c6-11e9-b210-d663bd873d93",
+      "category": "Array Prototype",
+      "img": "images/toString.png",
+      "correctAnswer": "toString",
+      "answerList": [
+        "filter",
+        "toString",
+        "splice"
+        ],
+      "docs": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/toString"
+    },
+    {
+      "id": "a7291f0a-36c6-11e9-b210-d663bd873d93",
+      "category": "Array Prototype",
+      "img": "images/unshift.png",
+      "correctAnswer": "unshift",
+      "answerList": [
+        "unshift",
+        "push",
+        "shift"
+        ],
+      "docs": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/unshift"
+    },
+    {
+      "id": "a7292194-36c6-11e9-b210-d663bd873d93",
+      "category": "Object Prototype",
+      "img": "images/Object-keys.png",
+      "correctAnswer": "Object.keys",
+      "answerList": [
+        "includes",
+        "Object.keys",
+        "Object.values"
+        ],
+      "docs": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys"
+    },
+    {
+      "id": "a7292428-36c6-11e9-b210-d663bd873d93",
+      "category": "Object Prototype",
+      "img": "images/Object-values.png",
+      "correctAnswer": "Object.values",
+      "answerList": [
+        "Object.values",
+        "Object.keys",
+        "map"
+        ],
+      "docs": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/values"
+    },
+    {
+      "id": "a7292694-36c6-11e9-b210-d663bd873d93",
+      "category": "String Prototype",
+      "img": "images/string-toUpperCase.png",
+      "correctAnswer": "toUpperCase",
+      "answerList": [
+        "toString",
+        "toUpperCase",
+        "toLowerCase"
+        ],
+      "docs": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase"
+    },
+    {
+      "id": "a729b3ca-36c6-11e9-b210-d663bd873d93",
+      "category": "String Prototype",
+      "img": "images/string-toLowerCase.png",
+      "correctAnswer": "toLowerCase",
+      "answerList": [
+        "toString",
+        "toUpperCase",
+        "toLowerCase"
+        ],
+      "docs": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toLowerCase"
+    },
+    {
+      "id": "a729b5e6-36c6-11e9-b210-d663bd873d93",
+      "category": "String Prototype",
+      "img": "images/string-split.png",
+      "correctAnswer": "split",
+      "answerList": [
+        "split",
+        "slice",
+        "replace"
+        ],
+      "docs": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split"
+    },
+    {
+      "id": "a729b83e-36c6-11e9-b210-d663bd873d93",
+      "category": "String Prototype",
+      "img": "images/string-slice.png",
+      "correctAnswer": "slice",
+      "answerList": [
+        "split",
+        "slice",
+        "concat"
+        ],
+      "docs": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/slice"
+    },
+    {
+      "id": "a729b992-36c6-11e9-b210-d663bd873d93",
+      "category": "String Prototype",
+      "img": "images/string-replace.png",
+      "correctAnswer": "replace",
+      "answerList": [
+        "replace",
+        "slice",
+        "concat"
+        ],
+      "docs": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace"
+    },
+    {
+      "id": "a729bb72-36c6-11e9-b210-d663bd873d93",
+      "category": "String Prototype",
+      "img": "images/string-match.png",
+      "correctAnswer": "match",
+      "answerList": [
+        "includes",
+        "slice",
+        "match"
+        ],
+      "docs": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/match"
+    },
+    {
+      "id": "a729bd2a-36c6-11e9-b210-d663bd873d93",
+      "category": "String Prototype",
+      "img": "images/string-includes.png",
+      "correctAnswer": "match",
+      "answerList": [
+        "includes",
+        "slice",
+        "match"
+        ],
+      "docs": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes"
+    },
+    {
+      "id": "a729bffa-36c6-11e9-b210-d663bd873d93",
+      "category": "String Prototype",
+      "img": "images/string-concat.png",
+      "correctAnswer": "concat",
+      "answerList": [
+        "slice",
+        "concat",
+        "replace"
+        ],
+      "docs": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/concat"
+    }
+  ]
+
+module.exports = {
+  proto-practice
+} 
