01e34ab67ce862ef67a42559e569aa1e479be45a
Kristen Hallstrom <klhalls89@gmail.com>
2019-02-25 09:46:20 -0700

    turn answeres into objects

--- datasets/kristen-hallstrom.js
+++ datasets/kristen-hallstrom.js
@@ -1,312 +1,271 @@
 let handbook = [
   { 
-    "id": "1",
     "question": "HTML stands for",
     "answers": [
-                "Hypertext Markup Language",
-                "Hypertext Moxie Language",
-                "Hyperbolic Markup Language",
-                "Hypertext Markup Links"
+                {"Correct": "Hypertext Markup Language"},
+                {"Incorrect": "Hypertext Moxie Language"},
+                {"Incorrect": "Hyperbolic Markup Language"},
+                {"Incorrect": "Hypertext Markup Links"}
     ] 
   },
   { 
-    "id": "2",
     "question": "People access websites through software called a",
     "answers": [
-                "Web browser",
-                "Web developer",
-                "Web researcher",
-                "Web controller"
+                {"Correct": "Web browser"},
+                {"Incorrect": "Web developer"},
+                {"Incorrect": "Web researcher"},
+                {"Incorrect": "Web controller"}
     ]
   },
   {
-    "id": "3",
     "question":  "Network request can be made with",
     "answers": [
-                "All of these",
-                "A url",
-                "Script tags",
-                "Fetch calls" 
+                {"Correct": "All of these"},
+                {"Incorrect": "A url"},
+                {"Incorrect": "Script tags"},
+                {"Incorrect": "Fetch calls"} 
     ]
   },
   { 
-    "id": "4",
     "question": "Network requests are made to a",
     "answers": [
-              "Web server",
-              "Web browser",
-              "Server farm",
-              "Web controller"
+                {"Correct": "Web server"},
+                {"Incorrect": "Web browser"},
+                {"Incorrect": "Server farm"},
+                {"Incorrect": "Web controller"}
     ]
   },
   { 
-    "id": "5",
     "question": "Arguably the hardest thing about front-end dev is",
     "answers": [
-              "Cross browser compatibility",
-              "Javascript",
-              "Hypertext Markup Language",
-              "Internet Explorer"
+                {"Correct": "Cross browser compatibility"},
+                {"Incorrect": "Javascript"},
+                {"Incorrect": "Hypertext Markup Language"},
+                {"Incorrect": "Internet Explorer"}
     ]
   },
   { 
-    "id": "6",
     "question": "Which of the following tags are non-semantic HTML",
     "answers": [
-                "<div> & <span>",
-                "<div> & <section>",
-                "<h1> & <span>",
-                "<p> & <form>"
+                {"Correct": "<div> & <span>"},
+                {"Incorrect": "<div> & <section>"},
+                {"Incorrect": "<h1> & <span>"},
+                {"Incorrect": "<p> & <form>"}
     ]
   },
   { 
-    "id": "7",
     "question": "Semantic HTML is",
     "answers": [
-                "All of these",
-                "Good for developer empathy",
-                "Good for screen readers",
-                "Easily forgotten in React"
+                {"Correct": "All of these"},
+                {"Incorrect": "Good for developer empathy"},
+                {"Incorrect": "Good for screen readers"},
+                {"Incorrect": "Easily forgotten in React"}
     ]
   },
   { 
-    "id": "8",
     "question": "CSS stands for",
     "answers": [
-                "Cascading Style Sheet",
-                "Computer Style Sheet",
-                "Computer Script System",
-                "Cascading Script System"
+                {"Correct": "Cascading Style Sheet"},
+                {"Incorrect": "Computer Style Sheet"},
+                {"Incorrect": "Computer Script System"},
+                {"Incorrect": "Cascading Script System"}
     ]
   },
   {
-    "id": "9",
     "question": "SCSS stands for",
     "answers": [
-                "Sassy Cascading Style Sheet",
-                "Stylish Computer Script Sheet",
-                "Sassy Computer Script System",
-                "Stylish Cascading Script System"
+                {"Correct": "Sassy Cascading Style Sheet"},
+                {"Incorrect": "Stylish Computer Script Sheet"},
+                {"Incorrect": "Sassy Computer Script System"},
+                {"Incorrect": "Stylish Cascading Script System"}
     ]
   },
   {
-    "id": "9",
     "question": "Which of the following are true about SCSS",
     "answers": [
-                "All of these",
-                "It’s SASS + CSS",
-                "It blends CSS with JS logic",
-                "It has less rigid syntax than SASS"
+                {"Correct":  "All of these"},
+                {"Incorrect": "It’s SASS + CSS"},
+                {"Incorrect": "It blends CSS with JS logic"},
+                {"Incorrect": "It has less rigid syntax than SASS"}
     ]
   },
   {
-    "id": "10",
     "question": "Which of these is the de facto language of the internet",
     "answers": [
-                "JavaScript",
-                "JSON",
-                "Java",
-                "CoffeeScript"
+                {"Correct":  "JavaScript"},
+                {"Incorrect": "JSON"},
+                {"Incorrect": "Java"},
+                {"Incorrect": "CoffeeScript"}
     ]
   },
   {
-    "id": "11",
     "question": "What is the other name for JavaScripts",
     "answers": [
-                "ECMAScript",
-                "JavaCoffee",
-                "CoffeeScript",
-                "Java"
+                {"Correct":  "ECMAScript"},
+                {"Incorrect": "JavaCoffee"},
+                {"Incorrect": "CoffeeScript"},
+                {"Incorrect": "Java"}
     ]
   },
   {
-    "id": "12",
     "question": "Which of the following are true about jQuery",
     "answers": [
-                "All of these",
-                "It is a JS library",
-                "the syntax is shorter then JS",
-                "It does everything JS can do"
+                {"Correct": "All of these"},
+                {"Incorrect": "It is a JS library"},
+                {"Incorrect": "the syntax is shorter then JS"},
+                {"Incorrect": "It does everything JS can do"}
     ]
   },
   {
-    "id": "13",
     "question": "What does DOM stand for",
     "answers": [
-                "Document Object Model",
-                "Digital Object Model",
-                "Document Object Manipulation",
-                "Document Object Manipulation"
+                {"Correct": "Document Object Model"},
+                {"Incorrect": "Digital Object Model"},
+                {"Incorrect": "Document Object Manipulation"},
+                {"Incorrect": "Document Object Manipulation"}
     ]
   },
   { 
-    "id": "14",
     "question":  "React uses a special kind of DOM, called",
     "answers": [
-                "A Virtual DOM",
-                "A Video DOM",
-                "A Reality DOM",
-                "A Viewing DOM"
+                {"Correct": "A Virtual DOM"},
+                {"Incorrect": "A Video DOM"},
+                {"Incorrect": "A Reality DOM"},
+                {"Incorrect": "A Viewing DOM"}
     ]
   },
   {
-    "id": "15",
     "question": "What is OOP",
     "answers": [
-                "Object Oriented Programing",
-                "Object Oriented Protocol",
-                "Operation Object Programing",
-                "Optional Object Protocol"
+                {"Correct": "Object Oriented Programing"},
+                {"Incorrect": "Object Oriented Protocol"},
+                {"Incorrect": "Operation Object Programing"},
+                {"Incorrect": "Optional Object Protocol"}
     ]
   },
-  { 
-    "id": "16",
-    "question": "What does AJAX stand for",
+  { "question": "What does AJAX stand for",
     "answers": [
-                "Asynchronous JavaScript And XML",
-                "Algorithm JavaScript And XML ",
-                "Asynchronous jQuery And XML",
-                "Asynchronous JavaScript Also XL"
+                {"Correct": "Asynchronous JavaScript And XML"},
+                {"Incorrect": "Algorithm JavaScript And XML "},
+                {"Incorrect": "Asynchronous jQuery And XML"},
+                {"Incorrect": "Asynchronous JavaScript Also XL"}
     ]
   },
   {
-    "id": "17",
     "question": "XML, though slightly outdated, stands for",
     "answers": [
-                "Extensible markup language",
-                "Exit markup language ",
-                "XL markup language",
-                "Existing markup language"
+                {"Correct": "Extensible markup language"},
+                {"Incorrect": "Exit markup language "},
+                {"Incorrect": "XL markup language"},
+                {"Incorrect": "Existing markup language"}
     ]
   },
   { 
-    "id": "18",
     "question": "JS is executed",
     "answers": [
-                "Synchronously",
-                "Asynchronously ",
-                "Congruently",
-                "Non-congruently"
+                {"Correct": "Synchronously"},
+                {"Incorrect": "Asynchronously "},
+                {"Incorrect": "Congruently"},
+                {"Incorrect": "Non-congruently"}
     ]
   },
   {
-    "id": "19",
     "question": "A fetch request is executed",
     "answers": [
-                "Asynchronousl",
-                "Synchronously",
-                "Congruently",
-                "Non-congruently"
+                {"Correct" : "Asynchronousl"},
+                {"Incorrect" : "Synchronously"},
+                {"Incorrect" : "Congruently"},
+                {"Incorrect" : "Non-congruently"}
     ]
   },
   { 
-    "id": "20",
     "question": "A fetch replaces what",
     "answers": [
-                "XHR, Xml Http Requests",
-                "Xmling",
-                "HR requests",
-                "Hyper linking"
+                {"Correct": "XHR, Xml Http Requests"},
+                {"Incorrect": "Xmling"},
+                {"Incorrect": "HR requests"},
+                {"Incorrect": "Hyper linking"}
     ]
   },
   {
-    "id": "21",
     "question": "JS classes are similar to these in React",
     "answers": [
-                "Components",
-                "Props ",
-                "State",
-                "Data"
+                {"Correct": "Components"},
+                {"Incorrect": "Props "},
+                {"Incorrect": "State"},
+                {"Incorrect": "Data"}
     ]
   },
   {
-    "id": "22",
     "question": "JS classes are similar to these in React",
     "answers": [
-                "Components",
-                "Props ",
-                "State",
-                "Data"
+                {"Correct": "Components"},
+                {"Incorrect": "Props "},
+                {"Incorrect": "State"},
+                {"Incorrect": "Data"}
     ]
   },
   { 
-    "id": "23",
     "question": "Classes and Components work best on SRP, which means",
     "answers": [
-                "Single Responsibility Principle",
-                "Syntax Response Principle ",
-                "Single Responsibility Program",
-                "Single Response paradigm"
+                {"Correct": "Single Responsibility Principle"},
+                {"Incorrect": "Syntax Response Principle "},
+                {"Incorrect": "Single Responsibility Program"},
+                {"Incorrect": "Single Response paradigm"}
     ]
   },
   {
-    "id": "24",
     "question": "The three basic languages of the internet are",
     "answers": [
-                "HTML, CSS & JS",
-                "HTML, SCSS & JS ",
-                "HTML, CSS & jQuery",
-                "XML, CSS & JS"
+                {"Correct": "HTML, CSS & JS"},
+                {"Incorrect": "HTML, SCSS & JS "},
+                {"Incorrect": "HTML, CSS & jQuery"},
+                {"Incorrect": "XML, CSS & JS"}
     ]
   },
   {
-    "id": "25",
     "question": "What is developer empathy",
     "answers": [
-                "Writing easy to follow code for other devs",
-                "Know what other devs must be feeling ",
-                "Group think based programing",
-                "Feeling for the end user"
+                {"Correct": "Writing easy to follow code for other devs"},
+                {"Incorrect": "Know what other devs must be feeling "},
+                {"Incorrect": "Group think based programing"},
+                {"Incorrect": "Feeling for the end user"}
     ]
   },
   { 
-    "id": "26",
     "question": "Who is a screen reader is used by",
     "answers": [
-                "All of these",
-                "The legally blind ",
-                "People with learning disabilities",
-                "The illiterate"
+                {"Correct": "All of these"},
+                {"Incorrect": "The legally blind "},
+                {"Incorrect": "People with learning disorders"},
+                {"Incorrect": "The illiterate"}
     ]
   },
   {
-    "id": "27",
     "question": "What direction does data flow in React",
     "answers": [
-                "Down",
-                "Up",
-                "All direction",
-                "Sibling to Sibling"
+                {"Correct": "Down"},
+                {"Incorrect": "Up"},
+                {"Incorrect": "All direction"},
+                {"Incorrect": "Sibling to Sibling"}
     ]
   },
   {
-    "id": "28",
     "question": "React was created by",
     "answers": [
-                "Facebook",
-                "Twitter ",
-                "Instagram",
-                "Pinterest"
+                {"Correct": "Facebook"},
+                {"Incorrect": "Twitter "},
+                {"Incorrect": "Instagram"},
+                {"Incorrect": "Pinterest"}
     ]
   },
   { 
-    "id": "29",
     "question": "What does every react page need at the top",
     "answers": [
-                "To import React",
-                "To import components from React ",
-                "To import React Dom",
-                "To import Data from React"
-    ]
-  },
-  { 
-    "id": "30",
-    "question": "What is the parent component in most React Apps",
-    "answers": [
-                "App",
-                "React",
-                "React Dom",
-                "Game"
+                {"Correct": "To import React"},
+                {"Incorrect": "To import components from React "},
+                {"Incorrect": "To import React Dom"},
+                {"Incorrect": "To import Data from React"}
     ]
   }
 ]
