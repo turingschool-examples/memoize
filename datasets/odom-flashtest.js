6dfbd1137fa6cb21d2757ebadb5dac358a642648
raechel <odom.raechel@gmail.com>
2019-02-24 21:41:50 -0700

    update to links

--- datasets/odom-flashtest.js
+++ datasets/odom-flashtest.js
@@ -3,180 +3,182 @@ const testCards = [
     "question": "What is T.D.D.?",
     "answer": "Test Driven Development",
     "type": "Multi",
-    "key": 1
+    "id": 1
   },
   {
     "question": "What are the four phases of testing?",
     "answer": "Setup, Execution, Assertion, & Teardown",
     "type": "Multi",
-    "key": 2
+    "id": 2
   },
   {
     "question": "What do we test?",
     "answer": "The outcome or result of a particular piece of code execution.",
     "type": "Multi",
-    "key": 3
+    "id": 3
   },
   {
     "question": "What is an Assertion Library?",
     "answer": "A package of assertion functionality.",
     "type": "Multi",
-    "key": 4
+    "id": 4
   },
   {
     "question": "What is a Testing Framework?",
     "answer": "A library that determines how tests are organized and executed.",
     "type": "Multi",
-    "key": 5
+    "id": 5
   },
   {
     "question": "What is the difference between a Testing Framework and Assertion Library?",
-    "answer": "",
+    "answer": "A testing library is a library that determines how tests are organized and a Library is a package of assertion functionality.",
     "type": "Multi",
-    "key": 6
+    "id": 6
   },
   {
     "question": "What happens in the Setup phase of testing?",
     "answer": "Setup the conditions required to execute the action on your tests.",
     "type": "Multi",
-    "key": 7
+    "id": 7
   },
   {
     "question": "What happens in the Execution phase of testing?",
-    "answer": "Execute some action on your test.",
+    "answer": "Do some action on your test.",
     "type": "Multi",
-    "key": 8
+    "id": 8
   },
   {
     "question": "What happens in the Assertion phase of testing?",
-    "answer": "Assert that the action you did had the results you expect.",
+    "answer": "Ensure that the action you did had the results you expect.",
     "type": "Multi",
-    "key": 9
+    "id": 9
   },
   {
     "question": "What happens in the Teardown phase of testing?",
     "answer": "Clean up any resources you used in your test (this is done automatically the majority of the time)",
     "type": "Multi",
-    "key": 10
+    "id": 10
   },
   {
     "question": "What happens in an Assertion?",
     "answer": "An expression containing some testable logic.",
     "type": "Multi",
-    "key": 11
+    "id": 11
   },
   {
     "question": "What is a Hook?",
     "answer": "A process that is kept outside the describe blocks and automatically runs before or after your tests as you've specified.",
     "type": "Multi",
-    "key": 12
+    "id": 12
   },
   {
     "question": "What is a Describe block?",
     "answer": "A block of code that encapsulates the order of operations for each inidividual smaller test within a suite.",
     "type": "Multi",
-    "key": 13
+    "id": 13
   },
   {
-    "question": "https://drive.google.com/file/d/18Rq4YYcmF8vmcijT_5pANP0JELKG6gJg/view",
+    "question": "https://i.ibb.co/1zsYCJ1/Screen-Shot-2019-02-22-at-10-36-14-AM-1.png",
     "answer": "assert",
     "type": "blank",
-    "key": 14
+    "id": 14
   },
   {
-    "question": "https://drive.google.com/file/d/1kKG1Zz9ECsZzeiD7Nsao2o1ZZoLfSfvB/view?usp=drive_open",
+    "question": "https://i.ibb.co/M7bbJVf/Screen-Shot-2019-02-22-at-10-40-22-AM.png",
     "answer": "pirate.",
     "type": "missing",
-    "key": 15
+    "id": 15
   },
   {
-    "question": "https://drive.google.com/file/d/1h-E5jcrMPdYmnt3o4LA2_y09Sc9ctjL4/view",
+    "question": "https://i.ibb.co/HYhXD1Y/Screen-Shot-2019-02-22-at-10-38-40-AM.png",
     "answer": "equal",
     "type": "blank",
-    "key": 16
+    "id": 16
   },
   {
-    "question": "https://drive.google.com/file/d/106nlLgOL4ke7qr3wef84EYK8YqUil3Tf/view",
+    "question": "https://i.ibb.co/1zsYCJ1/Screen-Shot-2019-02-22-at-10-36-14-AM-1.png",
     "answer": "Blackbeard",
     "type": "missing",
-    "key": 17
+    "id": 17
   },
   {
-    "question": "https://drive.google.com/file/d/1EeGyM7Il6md5FOWVH_6x32RLdgypqToC/view",
+    "question": "https://i.ibb.co/q5nHjJq/Screen-Shot-2019-02-22-at-10-55-24-AM.png",
     "answer": "Unicorn",
     "type": "blank",
-    "key": 18
+    "id": 18
   },
   {
-    "question": "https://drive.google.com/file/d/153eIlbqZ3ionD8jvQDCUzFlToakcBKcL/view",
+    "question": "https://i.ibb.co/j51YQdf/Screen-Shot-2019-02-22-at-11-01-35-AM.png",
     "answer": "deepEqual",
     "type": "blank",
-    "key": 19
+    "id": 19
   },
   {
-    "question": "https://drive.google.com/file/d/17kkeSw71_QZP-VoPC_rYMW3sjJMUFY_l/view",
+    "question": "https://i.ibb.co/4SbwCzd/Screen-Shot-2019-02-22-at-11-01-07-AM.png",
     "answer": "difference",
     "type": "blank",
-    "key": 20
+    "id": 20
   },
   {
-    "question": "https://drive.google.com/file/d/1Qo0HPZyke38EhkqFebEaezyoSP3_HTQG/view",
+    "question": "https://i.ibb.co/YjPSLKQ/Screen-Shot-2019-02-22-at-11-08-43-AM.png",
     "answer": "it",
     "type": "missing",
-    "key": 21
+    "id": 21
   },
   {
-    "question": "https://drive.google.com/file/d/1VK7zGgvwMB61NBaSBgsY3ETmXHfURTLg/view?usp=drive_open",
+    "question": "https://i.ibb.co/WyC7ktB/Screen-Shot-2019-02-22-at-11-20-40-AM.png",
     "answer": "isObject",
     "type": "blank",
-    "key": 22
+    "id": 22
   },
   {
-    "question": "https://drive.google.com/file/d/1ksfzJHvGhVGapcFmddj8Lbcwv9CGDaJW/view",
+    "question": "https://i.ibb.co/4RmcDr3/Screen-Shot-2019-02-22-at-11-32-37-AM.png",
     "answer": "simulate",
     "type": "blank",
-    "key": 23
+    "id": 23
   },
   {
-    "question": "https://drive.google.com/file/d/1q4O78uAfk4mf8UghcNFSGVgHk2wub3Xi/view",
+    "question": "https://i.ibb.co/16Kc6jz/Screen-Shot-2019-02-22-at-11-32-00-AM.png",
     "answer": "wrapper",
     "type": "blank",
-    "key": 24
+    "id": 24
   },
   {
-    "question": "https://drive.google.com/file/d/1wFRT95h0-otkK1ITDF0z5OYD5ZYmR80a/view",
+    "question": "https://i.ibb.co/BcFqtWD/Screen-Shot-2019-02-22-at-11-38-33-AM.png",
     "answer": "1",
     "type": "blank",
-    "key": 25
+    "id": 25
   },
   {
-    "question": "https://drive.google.com/open?id=1GnbI099_QMnFEMmJ2PqPjclMhx7T7VkP&authuser=0",
+    "question": "https://i.ibb.co/Cz51Wsh/Screen-Shot-2019-02-22-at-11-41-28-AM.png",
     "answer": "FilterControls",
     "type": "blank",
-    "key": 26
+    "id": 26
   },
   {
-    "question": "https://drive.google.com/file/d/1XhVPHZJOP-dgm5GenpXiM17Z0IUYFQ8y/view?usp=drive_open",
+    "question": "https://i.ibb.co/2jVw1MQ/Screen-Shot-2019-02-22-at-11-44-31-AM.png",
     "answer": ".toEqual",
     "type": "blank",
-    "key": 27
+    "id": 27
   },
   {
-    "question": "https://drive.google.com/file/d/1XhVPHZJOP-dgm5GenpXiM17Z0IUYFQ8y/view?usp=drive_open",
+    "question": "https://i.ibb.co/njbDpFr/Screen-Shot-2019-02-24-at-9-37-16-PM.png",
     "answer": "instance()",
     "type": "missing",
-    "key": 28
+    "id": 28
   },
   {
-    "question": "https://drive.google.com/file/d/1XQvw_KpUYXPrmhjJzp1phq4w3q6fpJPk/view",
+    "question": "https://i.ibb.co/ncmPM22/Screen-Shot-2019-02-22-at-11-52-35-AM.png",
     "answer": "find",
     "type": "blank",
-    "key": 29
+    "id": 29
   },
   {
-    "question": "https://drive.google.com/file/d/1QURGctYOmtLfMYL9HpMcwFg1_GMST-Vc/view",
+    "question": "https://i.ibb.co/R4hNsLq/Screen-Shot-2019-02-22-at-11-54-23-AM.png",
     "answer": "toMatchSnapshot()",
     "type": "blank",
-    "key": 30
+    "id": 30
   }
 ]
+
+export default testCards;
